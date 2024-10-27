66290b496019fded37b9b59e10803f72de88c88c
samumu1234 <75941684+samumu1234@users.noreply.github.com>
2024-10-27 14:42:28 +0100

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,17 +1,25 @@
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = htmlspecialchars($_POST['nombre']);
-    $email = htmlspecialchars($_POST['email']);
-    $mensaje = htmlspecialchars($_POST['mensaje']);
+    // Recoger y limpiar los datos del formulario
+    $nombre = trim(htmlspecialchars($_POST['nombre']));
+    $email = trim(htmlspecialchars($_POST['email']));
+    $mensaje = trim(htmlspecialchars($_POST['mensaje']));
 
+    // Validar el correo electrónico
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Correo electrónico no válido.";
+        exit;
+    }
+
+    // Configuración del correo
     $to = "bossbrayan51@gmail.com"; // Dirección de correo a la que se enviará el mensaje
-    $subject = "Nuevo mensaje de contacto";
+    $subject = "Nuevo mensaje de contacto de $nombre";
     $body = "Nombre: $nombre\nCorreo: $email\nMensaje:\n$mensaje";
-    $headers = "From: $email";
+    $headers = "From: $email\r\n" .
+               "Reply-To: $email\r\n" .
+               "X-Mailer: PHP/" . phpversion();
 
+    // Enviar el correo
     if (mail($to, $subject, $body, $headers)) {
         echo "Mensaje enviado con éxito.";
     } else {
